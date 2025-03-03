804b9e6128c927ac95954d2750659932f4cf0195
ysc <2725843507@qq.com>
2025-03-03 18:44:37 +0800

    modify task by disruptor & spotless & spotless

--- iotdb-collector/collector-core/src/main/java/org/apache/iotdb/collector/plugin/api/CollectorSource.java
+++ iotdb-collector/collector-core/src/main/java/org/apache/iotdb/collector/plugin/api/CollectorSource.java
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 package org.apache.iotdb.collector.plugin.api;
 
 import org.apache.iotdb.pipe.api.PipeSource;
